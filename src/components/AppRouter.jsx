560497a083cc89b7312d0b627ca0e1b2900be714
shami <Shamil79797@gmail.com>
2023-09-24 12:08:30 +0300

    Сделал posts

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,35 @@
+import React, {useContext} from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {privateRoutes, publicRoutes} from "../router";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
+
+const AppRouter = () => {
+    const {isAuth, isLoading} = useContext(AuthContext)
+    if (isLoading){
+        return <Loader/>
+    }
+    return (
+        isAuth
+            ? <Routes>
+                {privateRoutes.map(route => <Route
+                    key={route.path}
+                    element={route.component}
+                    path={route.path}
+                    exact={route.exact}
+                />)}
+                <Route path="/*" element={<Navigate to="/posts"/>}/>
+            </Routes>
+            : <Routes>
+                {publicRoutes.map(route => <Route
+                    key={route.path}
+                    element={route.component}
+                    path={route.path}
+                    exact={route.exact}
+                />)}
+                <Route path="/*" element={<Navigate to="/login"/>}/>
+            </Routes>
+    )
+};
+
+export default AppRouter;
